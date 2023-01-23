92b605c77abdb3785dc8f36eb07604a46184fab0
laurabragagnolo <laura.bragagnolo.1@studenti.unipd.it>
2023-01-23 17:44:40 +0100

    number of gpus is 1

--- test_LV_MHP_R101_75epoch.sh
+++ test_LV_MHP_R101_75epoch.sh
@@ -1,4 +1,5 @@
 CUDA_VISIBLE_DEVICES=2 python -u tools/test_net_parsing.py \
     --config-file "configs/LV_MHP/LV_R101_75epoch_IoUloss_IoUscore_parsing.yaml" \
     TEST.IMS_PER_BATCH 1 \
+    --num-gpus 1 \
     MODEL.WEIGHT ./checkpoints/LV-R101-75epoch-v100/model_final.pth
