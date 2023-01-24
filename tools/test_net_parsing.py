64d1060fbbb8f9e6c55a344456bf66345a238dfd
laurabragagnolo <laura.bragagnolo.1@studenti.unipd.it>
2023-01-24 10:00:07 +0100

    fixed comments

--- tools/test_net_parsing.py
+++ tools/test_net_parsing.py
@@ -39,8 +39,7 @@ def main():
 
     num_gpus = int(os.environ["WORLD_SIZE"]) if "WORLD_SIZE" in os.environ else 1
     multi_gpu_testing = True if num_gpus > 1 else False
-    print('#########################################################################')
-    print('#########################################################################')
+    print('### GPU info ###')
     print(multi_gpu_testing)
     print(torch.version.cuda)
 
