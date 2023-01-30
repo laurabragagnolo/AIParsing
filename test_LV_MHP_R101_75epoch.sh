1a1a5dc449a74b74a22c8bf59020b086ae225934
laurabragagnolo <laura.bragagnolo.1@studenti.unipd.it>
2023-01-30 12:08:03 +0100

    changed config file

--- test_LV_MHP_R101_75epoch.sh
+++ test_LV_MHP_R101_75epoch.sh
@@ -1,4 +1,4 @@
 python -u tools/test_net_parsing.py \
-    --config-file "configs/LV_MHP/LV_R101_75epoch_IoUloss_IoUscore_parsing.yaml" \
+    --config-file "configs/LV_MHP/LV_R101_75epoch_IoUloss_IoUscore_parsing_multiscale.yaml" \
     TEST.IMS_PER_BATCH 1 \
     MODEL.WEIGHT ./checkpoints/LV-R101-75epoch-v100/model_final.pth
