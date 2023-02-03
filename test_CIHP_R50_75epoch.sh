435e0d5bf878cb0350c98abd3aa371b646626159
laurabragagnolo <laura.bragagnolo.1@studenti.unipd.it>
2023-02-03 10:55:12 +0100

    bash file test on cihp dataset

--- test_CIHP_R50_75epoch.sh
+++ test_CIHP_R50_75epoch.sh
@@ -1,9 +1,7 @@
 
-
-
-CUDA_VISIBLE_DEVICES=0 python -u tools/test_net_parsing.py \
-		--config-file "configs/CIHP/CIHP_AIParsing_R50_75epoch_serialGE_parsingiou_iouloss.yaml" \
+python -u tools/test_net_parsing.py \
+		--config-file "configs/CIHP/CIHP_AIParsing_R101_75epoch_serialGE_parsingiou_iouloss.yaml" \
 		TEST.IMS_PER_BATCH 1 \
-		MODEL.WEIGHT ./checkpoints/CIHP_R50_75epoch/model_final.pth
+		MODEL.WEIGHT ./checkpoints/CIHP_R101_75epoch/model_final.pth
 
 
