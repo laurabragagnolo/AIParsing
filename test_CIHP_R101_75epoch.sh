a6fc2f011a7d68abe0ab0f1f4a1998047e0f95ca
laurabragagnolo <laura.bragagnolo.1@studenti.unipd.it>
2023-02-03 10:58:42 +0100

    bash file for test on cihp with res101

---
+++ test_CIHP_R101_75epoch.sh
@@ -0,0 +1,5 @@
+
+python -u tools/test_net_parsing.py \
+		--config-file "configs/CIHP/CIHP_AIParsing_R101_75epoch_serialGE_parsingiou_iouloss.yaml" \
+		TEST.IMS_PER_BATCH 1 \
+		MODEL.WEIGHT ./checkpoints/CIHP_R101_75epoch/model_final.pth
