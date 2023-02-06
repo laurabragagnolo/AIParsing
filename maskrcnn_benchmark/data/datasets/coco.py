1dfcdedc507ead4652809f7987e7eba786387744
laurabragagnolo <laura.bragagnolo.1@studenti.unipd.it>
2023-02-06 10:51:42 +0100

    print img info

--- maskrcnn_benchmark/data/datasets/coco.py
+++ maskrcnn_benchmark/data/datasets/coco.py
@@ -120,8 +120,7 @@ class COCODataset(torchvision.datasets.coco.CocoDetection):
         if 'parsing' in self.ann_types:
             parsing = [get_parsing(self.root, obj["parsing"]) for obj in anno]
             parsing = Parsing(parsing, img.size)
-            if parsing is None:
-                print(self.get_img_info(idx))
+            print(self.get_img_info(idx))
             target.add_field("parsing", parsing)
 
         target = target.clip_to_image(remove_empty=True)
